466c2490c9f060a6d81a994d98dfcb8860954e1d
Steven Topazio <sat@northpointprinting.com>
2020-02-11 20:04:39 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,9 +26,10 @@ WIN_COMBINATIONS = [
     
     if position_1 == "X" && position_2 == "X" && position_3 == "X" 
       return win_combo 
-    else
-      FALSE
-    end
+    end 
+    }
+    return FALSE
   end
+  
   
       
