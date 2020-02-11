8c70ba87f4b1a9f9a2271c8964365162bfe80a61
Steven Topazio <sat@northpointprinting.com>
2020-02-11 20:13:29 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -54,6 +54,19 @@ WIN_COMBINATIONS = [
     end
   end
   
+  def winner (board)
+    index = []
+    index = won?(board)
+    if index == false 
+      return nil
+    else
+      if board[index[0]] == "X"
+        return "X"
+      else 
+        return "O"
+      end
+    end
+  end
   
   
   
